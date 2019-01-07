7482db89b775fc0ff3a4390b588f68c953f347a6
WilliamPapirose <WilliamPapirose@users.noreply.github.com>
2019-01-07 08:06:52 +0600

    popup

--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -38,36 +38,38 @@ const CardInfo = ({
             </button>
           </div>
         </div>
-        {card.withDescription && (
-          <div>
-            <Description
-              addDescription={addDescription}
-              changeDescription={changeDescription}
-              saveDescription={saveDescription}
-              card={card}
-            />
-          </div>
-        )}
-        {(!card.withDescription && card.editable) && (
-          <div className="description">
-            <button
-              type="button"
-              className="button plus"
-              onClick={() => {
-                addDescription(true);
-              }}
-            >
-              Add description
-            </button>
-          </div>
-        )}
-        <CommentForm
-          user={user}
-          card={card}
-          addComment={addComment}
-          edit={editComment}
-          deleteComment={deleteComment}
-        />
+        <div className="information">
+          {card.withDescription && (
+            <div>
+              <Description
+                addDescription={addDescription}
+                changeDescription={changeDescription}
+                saveDescription={saveDescription}
+                card={card}
+              />
+            </div>
+          )}
+          {(!card.withDescription && card.editable) && (
+            <div className="description">
+              <button
+                type="button"
+                className="button plus"
+                onClick={() => {
+                  addDescription(true);
+                }}
+              >
+                Add description
+              </button>
+            </div>
+          )}
+          <CommentForm
+            user={user}
+            card={card}
+            addComment={addComment}
+            edit={editComment}
+            deleteComment={deleteComment}
+          />
+        </div>
       </div>
     </div>
   </div>
